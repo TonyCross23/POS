016d8948511349f337b6cb978c90399bdb1b485c
TonyCross23 <tonychamp40@gmail.com>
2024-09-28 13:49:10 +0630

    product-end

---
+++ app/Filament/Resources/ProductResource.php
@@ -0,0 +1,181 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use App\Models\Product;
+use Filament\Forms\Set;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Illuminate\Support\Str;
+use Filament\Resources\Resource;
+use Illuminate\Support\HtmlString;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Components\Section;
+use Filament\Tables\Actions\ActionGroup;
+use Filament\Tables\Filters\SelectFilter;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Forms\Components\MarkdownEditor;
+use App\Filament\Resources\ProductResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\ProductResource\RelationManagers;
+
+class ProductResource extends Resource
+{
+    protected static ?string $model = Product::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-squares-2x2';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+          ->schema([
+           Section::make([
+            Grid::make()->schema([
+                Group::make()->schema([
+                    Section::make("Product information")->schema([
+                            Forms\Components\TextInput::make('name')
+                                ->required()
+                                ->helperText(new HtmlString('Input your product name'))
+                                ->maxLength(255)
+                                ->live()
+                                ->afterStateUpdated(fn(string $operation, $state, Set $set) => $operation === 'create' ? $set('slug', Str::slug($state)) : null),
+                            Forms\Components\TextInput::make('slug')
+                                ->required()
+                                ->disabled()
+                                ->dehydrated()
+                                ->helperText(new HtmlString('If you input name slug is auto filed this name'))
+                                ->maxLength(255)
+                                ->unique(Product::class,'slug', ignoreRecord:true),
+                            MarkdownEditor::make('description')
+                                ->columnSpanFull()
+                                ->fileAttachmentsDirectory('products'),
+                    ])->columnSpan(2),
+                    Section::make('image')->schema([
+                        Forms\Components\FileUpload::make('image')
+                        ->image()
+                        ->multiple()
+                        ->maxFiles(5)
+                        ->reorderable()
+                        ->directory('products'),
+                    ])
+                 ])->columnSpan(2)
+            ])
+           ])->columnSpan(2),
+
+           Group::make()->schema([
+                Section::make('Price')->schema([
+                    Forms\Components\TextInput::make('price')
+                        ->required()
+                        ->numeric()
+                        ->prefix('$'),
+                ]),
+
+                Section::make('Associations')->schema([
+                    Select::make('category_id')
+                        ->required()
+                        ->searchable()
+                        ->preload()
+                        ->relationship('category', 'name'),
+                    Select::make('brand_id')
+                        ->required()
+                        ->searchable()
+                        ->preload()
+                        ->relationship('brand', 'name'),
+                ]),
+                Section::make('Status')->schema([
+                    Toggle::make('is_active')
+                        ->required()
+                        ->default(true),
+                    Toggle::make('in_stock')
+                        ->required()
+                        ->default(true),
+                    Toggle::make('is_featured')
+                        ->required(),
+                    Toggle::make('on_sale')
+                        ->required(),
+                ])
+           ])->columnSpan(1)
+
+          ])->columns(3);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\ImageColumn::make('image'),
+                Tables\Columns\TextColumn::make('category.name')
+                    ->numeric()
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('brand.name')
+                    ->numeric()
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('price')
+                    ->money()
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\IconColumn::make('is_active')
+                    ->boolean(),
+                Tables\Columns\IconColumn::make('is_featured')
+                    ->boolean(),
+                Tables\Columns\IconColumn::make('in_stock')
+                    ->boolean(),
+                Tables\Columns\IconColumn::make('on_sale')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                SelectFilter::make('category')
+                    ->searchable()
+                    ->relationship('category','name'),
+                SelectFilter::make('brand')
+                    ->searchable()
+                    ->relationship('brand','name'),
+            ])
+            ->actions([
+                ActionGroup::make([
+                    Tables\Actions\ViewAction::make(),
+                    Tables\Actions\EditAction::make(),
+                    Tables\Actions\DeleteAction::make(),
+                ])
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProducts::route('/'),
+            'create' => Pages\CreateProduct::route('/create'),
+            'edit' => Pages\EditProduct::route('/{record}/edit'),
+        ];
+    }
+}
