bf211979db598b265ae0277d1f36feacb7f0e173
TonyCross23 <tonychamp40@gmail.com>
2024-10-27 20:40:51 +0630

    payment_fix

--- app/Livewire/Page/SuccessPage.php
+++ app/Livewire/Page/SuccessPage.php
@@ -2,12 +2,40 @@
 
 namespace App\Livewire\Page;
 
+use Stripe\Stripe;
+use App\Models\Order;
 use Livewire\Component;
+use Livewire\Attributes\Url;
+use Stripe\Checkout\Session;
+use Livewire\Attributes\Title;
+
+#[Title('Success - Prime Shop')]
 
 class SuccessPage extends Component
 {
+    #[Url]
+    public $session_id;
+
     public function render()
     {
-        return view('livewire.page.success-page');
+        $latest_order = Order::with('address')->where('user_id',auth()->user()->id)->latest()->first();
+
+        if($this->session_id) {
+            Stripe::setApiKey(env("STRIPE_KEY"));
+            $session_info = Session::retrieve($this->session_id);
+
+            if($session_info->payment_status != 'paid') {
+                $latest_order->payment_status = 'failed';
+                $latest_order->save();
+                return redirect()->route('cancel');
+            }else if($session_info->payment_status == 'paid') {
+                $latest_order->payment_status = 'paid';
+                $latest_order->save();
+            }
+        }
+
+        return view('livewire.page.success-page',[
+            'order' => $latest_order,
+        ]);
     }
 }
